58a1b9497f2dda356bf4ed6826d07889ec6f50e6
Jeff Eaton <eaton@nerdhaus.net>
2020-11-26 12:23:35 -0600

    Improved regex
    More tolerant of whitespace scenarios, ignores footnote *markers* because they’re someone else’s problem, and handles multi-line footnote content with aplomb.
    
    Downside: Inline footnotes with bracketed markdown (`[^ Like so: ![Image Alt](image.jpg Title Text)]`) very much don’t work. Can’t figure out a good way to solve that without breaking paragraphs that have multiple inline footnotes, or switching to something more exotic than ECMA regexes; its inability to do balancing/recursion makes it a real mess. For now, we’ll deal.

--- src/admin/sidenote.js
+++ src/admin/sidenote.js
@@ -9,24 +9,54 @@ CMS.registerEditorComponent({
     { name: "content", label: "Sidenote content", widget: "text", minimal: true, buttons: ['bold', 'italic', 'link'], editor_components: [] },
   ],
   
-  // Pattern to identify a block as being an instance of this component
-  pattern: /(\[\^([0-9a-zA-Z]*)]:(?:\s*)?({-})?(?: *)?(\S.*)|\[\^ ({-})?(?: *)?(\S.*)])/,
+  /*
+		This regex parses out the notes-containing portions of all tufte-markdown
+		(https://github.com/luhmann/tufte-markdown/tree/master/packages/remark-sidenotes/src)
+		flavored sidenotes and margin-notes:
+
+			[^1]: Referenced sidenote
+			[^1]: Referenced sidenotes w/markdown `![Image Alt](image.jpg Title Text)`
+			[^1]: Referenced sidenotes with multi-line contents, as long as each line is
+			  indented by at least two spaces
+			[^1]: {-} Margin-note variations of the above formats
+
+			[^ Inline sidenote]
+			[^ {-} Inline margin-note]
+
+			Text with[^ multiple] inline side and margin notes[^{-} in one paragraph] as well.
+		
+    Nested markdown inside an inline footnote breaks atm; EMCAScript regexes don't support 
+    balancing or lookaheads, which makes any nested markdown… fussy. Going greedy makes
+    the match below work, but breaks multiple simpler inlines inside a single paragraph,
+    which feels like a better scenario to optimize for ATM.
+    
+			[^ Like so: `![Image Alt](image.jpg Title Text)`]      <-- Breaks ATM
+			[^ {-} Like so: `![Image Alt](image.jpg Title Text)`]   <-- Breaks ATM
+    
+		Due to the way the pattern works, referenced side/margin note results appear in match
+		results 1, 2, and 3 while inline side and margin-notes appear in matches 4 and 5.
+		If we were using PCRE we could get fancier and use duplicated named groups, but that
+		is not our lot in life; ECMA regexes don't support the (?J) directive to allow
+		duplicate named groups. Alas.
+   */
+   
+  pattern: /\[\^([[0-9a-zA-Z]*)]:(?:\s*)({-})?(?: *)?(\S(?:.|(?:\n {2,}))*)|\[\^\s({-})?(?: *)(\S[^\]]*)]/,
   
   // Function to extract data elements from the regexp match
   fromBlock: function (match) {
-    if (match[2]) {
+    if (match[1]) {
       // An ID was given, e.g. `[^1]: This`
 			return {
-				id: match[2],
-				margin: match[3],
-				content: match[4]
+				id: match[1],
+				margin: match[2],
+				content: match[3]
 			};
     } else {
       // Inline referenceless version, e.g. `[^ This]`
 			return {
 				id: false,
-				margin: match[5],
-				content: match[6]
+				margin: match[4],
+				content: match[5]
 			};
     }
   },
