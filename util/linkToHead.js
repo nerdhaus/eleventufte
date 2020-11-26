92ae5bd64fa1666813ac6e411d5719976a9562b8
Jeff Eaton <eaton@nerdhaus.net>
2020-11-26 15:11:54 -0600

    Fixing some header inclusion issues.

--- util/linkToHead.js
+++ util/linkToHead.js
@@ -1,17 +1,23 @@
 /* We use this to convert attachments to links in the document head */
 
 module.exports = function (files, local=false) {
+	if (!Array.isArray(files)) files = Array(files)
 	let items = [];
 	files.forEach((item) => {
-		if (item.endsWith(".js")) {
-			if (local) {
-				items.push('<script src="' + item + '"></script>')
-			} else {
-				items.push('<script crossorigin="anonymous" src="' + item + '"></script>')
-			}
-		} else if (item.endsWith(".css")) {
-			items.push('<link rel="stylesheet" href="' + item + '">')
-		}
+		if (x = itemToLink(item, local)) items.push(x)
 	});
 	return items.join("\n");
 };
+
+function itemToLink(item, local) {
+	if (item.endsWith(".js")) {
+		if (local) {
+			return '<script src="' + item + '"></script>'
+		} else {
+			return '<script crossorigin="anonymous" src="' + item + '"></script>'
+		}
+	} else if (item.endsWith(".css")) {
+		return '<link rel="stylesheet" href="' + item + '">'
+	}
+	return false;
+}
